395ca113ed3f6a0a545c18f642eaf24eacd13a5e
sergiova123 <159633516+sergiova123@users.noreply.github.com>
2024-02-17 15:27:47 +0000

    codigo clase 2

--- clase1/main.py
+++ clase1/main.py
@@ -1,1 +1,14 @@
-print("primer codigo en python")
+class MotorElectrico:
+    potencia = None
+
+    def encender(self):
+        print("Encendido de motor Electrico")
+
+    def __ init __ (self):
+        pass
+
+motor1 = MotorElectrico()
+motor1.potencia = 1000
+motor1.encender()
+
+
