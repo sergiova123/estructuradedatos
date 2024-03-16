94cc715fa5f64bd47d59c1fdc2715b9297f45cba
sergiova123 <159633516+sergiova123@users.noreply.github.com>
2024-03-16 15:40:19 +0000

    trabajo_colas

--- clase1/trabajo_colas/classes/VehicleManager.py
+++ clase1/trabajo_colas/classes/VehicleManager.py
@@ -1,33 +1,32 @@
-from collections import deque
+from queue import Queue
 from models import Vehicle
 import random
 
 class VehicleManager:
     def __init__(self):
-        self.vehicles_to_be_tested = deque()
+        self.vehicles_to_be_tested = Queue()
         self.tested_vehicles = []
         self.vehicle_lookup = {}
 
     def add_vehicle(self, vehicle: Vehicle):
-        self.vehicles_to_be_tested.append(vehicle)
+        self.vehicles_to_be_tested.put(vehicle)
         self.vehicle_lookup[vehicle.tuition] = vehicle
 
     def vehicle_tested(self, tuition: str):
         vehicle = self.vehicle_lookup.get(tuition)
         if vehicle:
-            self.vehicles_to_be_tested.remove(vehicle)
-            self.tested_vehicles.append(vehicle)
             del self.vehicle_lookup[tuition]
+            self.tested_vehicles.append(vehicle)
 
     def get_vehicles_to_be_tested(self):
-        return list(self.vehicles_to_be_tested)
+        return list(self.vehicles_to_be_tested.queue)
 
     def get_tested_vehicles(self):
         return self.tested_vehicles
     
     def execute_tests_for_vehicle(self):
-        if self.vehicles_to_be_tested:
-            vehicle = self.vehicles_to_be_tested.popleft()
+        if not self.vehicles_to_be_tested.empty():
+            vehicle = self.vehicles_to_be_tested.get()
             del self.vehicle_lookup[vehicle.tuition]
             for test in vehicle.tests:
                 test.result = random.choice([True, False])
@@ -38,7 +37,6 @@ class VehicleManager:
     def remove_pending_vehicle(self, tuition: str):
         vehicle = self.vehicle_lookup.get(tuition)
         if vehicle:
-            self.vehicles_to_be_tested.remove(vehicle)
             del self.vehicle_lookup[tuition]
             return True
         return False
