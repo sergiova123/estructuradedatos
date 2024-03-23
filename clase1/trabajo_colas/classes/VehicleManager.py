cd877e8fb904a9ec714192797e91afc0b057427f
sergiova123 <159633516+sergiova123@users.noreply.github.com>
2024-03-23 14:10:41 +0000

    final

--- clase1/trabajo_colas/classes/VehicleManager.py
+++ clase1/trabajo_colas/classes/VehicleManager.py
@@ -6,39 +6,23 @@ class VehicleManager:
     def __init__(self):
         self.vehicles_to_be_tested = Queue()
         self.tested_vehicles = []
-        self.vehicle_lookup = {}
 
     def add_vehicle(self, vehicle: Vehicle):
         self.vehicles_to_be_tested.put(vehicle)
-        self.vehicle_lookup[vehicle.tuition] = vehicle
 
-    def vehicle_tested(self, tuition: str):
-        vehicle = self.vehicle_lookup.get(tuition)
-        if vehicle:
-            del self.vehicle_lookup[tuition]
+    def vehicle_tested(self):
+        if not self.vehicles_to_be_tested.empty():
+            vehicle = self.vehicles_to_be_tested.get()
+            for test in vehicle.tests:
+                test.result = random.choice([True, False])
             self.tested_vehicles.append(vehicle)
+            return vehicle
+        return None
 
     def get_vehicles_to_be_tested(self):
         return list(self.vehicles_to_be_tested.queue)
 
     def get_tested_vehicles(self):
         return self.tested_vehicles
-    
-    def execute_tests_for_vehicle(self):
-        if not self.vehicles_to_be_tested.empty():
-            vehicle = self.vehicles_to_be_tested.get()
-            del self.vehicle_lookup[vehicle.tuition]
-            for test in vehicle.tests:
-                test.result = random.choice([True, False])
-            self.tested_vehicles.append(vehicle)
-            return vehicle
-        return None
-    
-    def remove_pending_vehicle(self, tuition: str):
-        vehicle = self.vehicle_lookup.get(tuition)
-        if vehicle:
-            del self.vehicle_lookup[tuition]
-            return True
-        return False
 
 vehicle_manager = VehicleManager()
