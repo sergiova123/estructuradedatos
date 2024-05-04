565ad16a17de9a99f89c9f6aa80642949e0a2ae5
sergiova123 <159633516+sergiova123@users.noreply.github.com>
2024-05-04 00:20:47 +0000

    03/05

--- clase1/colas2/paquete/cola.py
+++ clase1/colas2/paquete/cola.py
@@ -14,21 +14,21 @@ class Cola:
         return self.primero is None
 
     def encolar(self, valor):
-        nuevo_nodo = Nodo(valor)
+        nuevo_nodo = Nodo(valor) #Esto crea un nuevo nodo que contendrá el valor pasado como argumento.
         print(nuevo_nodo)
         #nuevo_nodo.valor.pruebas = self.agregar_pruebas_tecnicas(nuevo_nodo.valor.tipo)
 
-        if self.esta_vacia():
-            self.primero = nuevo_nodo
-        else:
-            self.ultimo.siguiente = nuevo_nodo
-        self.ultimo = nuevo_nodo
+        if self.esta_vacia(): #Si la cola está vacía, significa que este será el primer nodo en la cola.
+            self.primero = nuevo_nodo #Si la cola está vacía, se asigna el nuevo nodo como el primer nodo
+        else: #Si la cola no está vacía, significa que ya hay al menos un nodo en la cola.
+            self.ultimo.siguiente = nuevo_nodo #En este caso, el nuevo nodo se añade al final de la cola. La referencia
+        self.ultimo = nuevo_nodo #Se actualiza la referencia ultimo para que apunte al nuevo nodo,
     
     def desencolar(self):
         if self.esta_vacia():
-            return None
-        valor = self.primero.valor
-        self.primero = self.primero.siguiente
+            return None # None), el método devuelve None, indicando que no hay nada que desencolar.
+        valor = self.primero.valor 
+        self.primero = self.primero.siguiente #esto es necesario porque el método devolverá este valor después de desencolarlo.
         if self.primero is None:
             self.ultimo = None
         return valor
@@ -53,7 +53,7 @@ class Cola:
             while current:
                 elementos.append(current.valor)
                 current = current.siguiente
-            return elementos
+            return elementos    
 
     def ver_primero(self):
         if self.esta_vacia():
